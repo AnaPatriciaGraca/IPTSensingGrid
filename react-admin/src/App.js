a659bd43fbaa439090da8aee8e7e350d46614c2a
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-08-01 18:28:02 +0100

    Finish theme prep
    Only missing the button to toggle the modes

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,8 +1,19 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode()
+
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* Reset the CSS to defaults */}
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
