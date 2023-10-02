faba3bb3105691b0f6eb1d72fb60c92350213f52
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-10-02 20:54:47 +0100

    Pop ups da reserva da sala

--- react-admin/src/components/SearchResult.js
+++ react-admin/src/components/SearchResult.js
@@ -1,11 +1,16 @@
-import { Box, Typography, Table, TableHead, TableBody, styled, TableRow, TableContainer, Paper } from '@mui/material'
+import { Box, Typography, Table, TableHead, TableBody, styled, TableRow, TableContainer, Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material'
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import { tokens } from '../theme';
 import { useTheme } from '@mui/material';
+import { useState } from 'react';
 
 const SearchResult = ({data}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
+    const [isDialogOpen, setIsDialogOpen] = useState('')
+    const [selectedRoom, setSelectedRoom] = useState('')
+    const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+
     const StyledTableCell = styled(TableCell)(() => ({
         [`&.${tableCellClasses.head}`]: {
           backgroundColor: colors.blueAccent[400],
@@ -33,6 +38,25 @@ const SearchResult = ({data}) => {
           },
     }));
 
+    //event when I click a Room (row)
+    const handleRowClick = (room) => {
+        setSelectedRoom(room);
+        setIsDialogOpen(true);
+    };
+
+    //modify this function to actually do the reserve of the room
+    const handleReserve = () => {
+        setIsConfirmationOpen(true);
+      };
+    
+    //close pop ups
+    const handleCloseDialog = () => {
+        setSelectedRoom(null);
+        setIsDialogOpen(false);
+        setIsConfirmationOpen(false);
+            
+    }; 
+
     return (
         <Box mt='40px'>
             <Typography mb='10px' variant="h6">Resultados da Pesquisa:</Typography>
@@ -49,7 +73,7 @@ const SearchResult = ({data}) => {
                     </TableHead>
                     <TableBody>
                     {data.map((row) => (
-                        <StyledTableRow key={row.name}>
+                        <StyledTableRow key={row.id} onClick={() => handleRowClick(row)}>
                         <StyledTableCell align="left">{row.function}</StyledTableCell>
                         <StyledTableCell component="th" scope="row">{row.name}</StyledTableCell>
                         <StyledTableCell align="center">{row.projector}</StyledTableCell>
@@ -60,6 +84,79 @@ const SearchResult = ({data}) => {
                     </TableBody>
                 </Table>
             </TableContainer>
+            
+            {/* Pop up to reserv the room*/}
+            <Dialog open={isDialogOpen} onClose={handleCloseDialog}>
+                <DialogTitle color={colors.greenAccent[400]} fontWeight='bold' fontSize={16}>Confirmar Reserva</DialogTitle>
+                <DialogContent>
+                    {selectedRoom && (
+                    <Typography>
+                        Tem a certeza de que quer reservar a sala {selectedRoom.name}?
+                    </Typography>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={handleCloseDialog}
+                        color="primary"
+                        sx={{
+                            background: colors.greenAccent[400],
+                            fontWeight: 'bold',
+                            fontSize: 12,
+                            '&:hover': {
+                            background: colors.greenAccent[600], 
+                            },
+                        }}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        onClick={handleReserve}
+                        color="primary"
+                        sx={{
+                            background: colors.greenAccent[400],
+                            fontWeight: 'bold',
+                            fontSize: 12,
+                            '&:hover': {
+                            background: colors.greenAccent[600], 
+                            },
+                        }}
+                    >
+                        Reserve
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* Pop up to confirm the reserve */}
+            <Dialog open={isConfirmationOpen} onClose={handleCloseDialog}>
+                <DialogTitle color={colors.greenAccent[400]} fontWeight='bold' fontSize={16}>Confirmação</DialogTitle>
+                <DialogContent>
+                    {selectedRoom && (
+                    <Typography>
+                        Sala {selectedRoom.name} foi reservada
+                    </Typography>
+                    )}
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={handleCloseDialog}
+                        color="primary"
+                        sx={{
+                            background: colors.greenAccent[400],
+                            fontWeight: 'bold',
+                            fontSize: 12,
+                            '&:hover': {
+                            background: colors.greenAccent[600], 
+                            },
+                        }}
+                    >
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+
+
         </Box>
         
     )
